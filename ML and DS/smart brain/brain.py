967658b64148caf4ef582571a546ca3381c26b3c
Nguyen <babyfacedeveloper@gmail.com>
2022-12-31 19:27:29 -0500

    ml project

---
+++ ML and DS/smart brain/brain.py
@@ -0,0 +1,13 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsInceptionV3()
+prediction.setModelPath(os.path.join(execution_path, "inception_v3_google-1a9a5a14.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "godzilla.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
